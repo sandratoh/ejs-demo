1ffb810799d93fdb236402048db131c91118bd8f
Sandra To <sandra.to.e@gmail.com>
2021-02-15 21:17:17 +0000

    Linted codes

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // load the things we need
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-// index page 
+// index page
 app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+  let mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  let tagline = "No programming concept is complete without a cute animal mascot.";
 
-    res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline
-    });
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 // about page
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+  res.render('pages/about');
 });
 
 app.listen(8080);
